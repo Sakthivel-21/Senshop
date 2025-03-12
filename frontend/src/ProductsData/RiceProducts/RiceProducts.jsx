4b73c1b5ac3e45979672c8e8016236255e80fb9f
sakthi <sakthivelkalidass@gmail.com>
2025-03-13 00:52:45 +0530

    rice image

--- frontend/src/ProductsData/RiceProducts/RiceProducts.jsx
+++ frontend/src/ProductsData/RiceProducts/RiceProducts.jsx
@@ -47,7 +47,7 @@ function RiceProducts({searchTerm}) {
 <div className='rice-products'>
 
    
-     <img src={'http://localhost:3001/'+(product.image)} alt=''></img>
+     <img src={`${process.env.REACT_APP_API_URL}/`+(product.image)} alt=''></img>
    
     <div className='rice-products-price'>
      <h2 className='cate'>{product.category}</h2>
