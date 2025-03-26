13be02d673fff1512b5ca59cb56139d69b459887
sakthi <sakthivelkalidass@gmail.com>
2025-03-26 14:52:12 +0530

    usersdataupdate

--- frontend/src/Admin/UserOrders/UserOrders.jsx
+++ frontend/src/Admin/UserOrders/UserOrders.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './UserOrders.css'
+import axios from 'axios'
+import {format} from "date-fns";
+import { Link } from 'react-router-dom';
+
+
 
 function UserOrders() {
+
+   axios.defaults.withCredentials= true;
+  
+    const[user, setUser] = useState()
+
+  
+      useEffect (() => {
+          axios.get(`${process.env.REACT_APP_API_URL}/dashboard`)
+             .then((response) => {
+               setUser(response.data);
+             })
+             .catch((err) => console.log(err))
+     }, [])
+  
+     if(!user) {
+      return <h4>loading.....</h4>
+  }
+  
+  
   return (
-    <div>UserOrders</div>
+   <>
+     <h2 className='user-head'>Your Orders</h2>
+        <hr className='hr-lineded'/>
+     
+       
+     
+       <div className='user'>
+       { user.map(booking => (
+         <>
+        <Link to={`/bookingpage/${booking._id}`} className='user-link'><div className='user-container'>
+     
+             
+               <img src={booking.places.place.image} alt=''></img>
+     
+               <p className='user-category'>Category:  {booking.places.place.category}</p>
+             
+               <p className='user-1'>Total Price: RS {booking.places.price}</p>
+              
+               <p className='user-1'>Email:  {booking.user.email}</p>
+              
+              
+
+             
+     
+        </div></Link>
+        <hr className='hr-user'/>
+        </>
+     ))} 
+     </div>
+   </>
   )
 }
 
